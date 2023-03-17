cd0ccbbf4c77628d9422dbc39f23e45136b5b8d8
KGahan <kimgahan05@gmail.com>
2023-03-17 10:13:45 +0100

    css finished, js 1-2

---
+++ js/01_variablen-operatoren/variablen-operatoren.js
@@ -0,0 +1,28 @@
+console.log("hellou")
+
+let a = 7;
+let b = 8;
+let c = 10;
+
+let result = a+b+c;
+console.log(result)
+
+result = (a + b) / c;
+console.log(result)
+
+result = c -a + b;
+console.log(result)
+
+result = a + b + c * 3;
+console.log(result)
+
+result =  (a + b + c)/4;
+console.log(result)
+
+a = 3
+b = a + 3
+c = c -3
+
+console.log(a)
+console.log(b)
+console.log(c)
---
+++ js/02_strings-zahlen/strings-zahlen.js
@@ -0,0 +1,6 @@
+let result = 17.4 / 3.9; 
+let vorname = "Kim";
+let nachname ="Gahan";
+
+console.log("The result of 17.4 divided by 3.9 is " + result);
+console.log("Ich heiße " + vorname +" " + nachname);
